6febab7b026bee50c499646b06a2969172011df1
DiegoPanno <diegompanno@gmail.com>
2025-03-26 11:13:37 -0300

    correccion

--- src/mercadoLibreAuth.js
+++ src/mercadoLibreAuth.js
@@ -18,21 +18,3 @@ export const obtenerAccessToken = async (code) => {
 };
 
   
-    return fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams(body),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        return data.access_token;  // Regresa el access_token
-      })
-      .catch((error) => {
-        console.error("Error al obtener el access token:", error);
-        throw error;
-      });
-  };
-  
-  
